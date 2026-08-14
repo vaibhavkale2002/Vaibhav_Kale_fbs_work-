c8fe4c2bcc6897c9df5b725c2eb90237dbab2457
vaibhavkale2002 <deshmukhvaibhav40872@gmail.com>
2026-08-14 18:01:58 +0530

    Add files via upload

---
+++ test_1.py
@@ -0,0 +1,38 @@
+# Q1 Write a program to find the area and perimeter of following figure (Accept the
+# length, breadth and radius from user:
+
+# length=int(input('enter the number'))
+# breadth=int(input('enter the number '))
+# radius=float(input('enter the number'))
+
+# arearectangle=length*breadth
+# circlearea=0.5*3.14*radius**2
+# area=arearectangle+circlearea
+
+# perimeter=(2*length)+ breadth+3.14*radius
+
+# print('area=',area)
+# print('perimeter=',perimeter)
+
+#Q2.Write a program to calculate simple interest based on Principal, Rate and Time
+#(SI = P*R*T/100)
+
+
+# P = int(input("Enter Principal: "))
+# R = int(input("Enter Rate: "))
+# T = int(input("Enter Time: "))
+
+# SI = (P * R * T) / 100
+
+# print("Simple Interest =", SI)
+
+#Q3
+# km = float(input("Enter distance in kilometers: "))
+
+# meters = km * 1000
+# centimeters = km * 100000
+
+# print("Distance in meters =", meters)
+# print("Distance in centimeters =", centimeters)
+
+#Q4
