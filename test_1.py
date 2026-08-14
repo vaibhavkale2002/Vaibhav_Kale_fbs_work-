23c4acee7529a796aa6d7179d1a2a6d2ee7609b3
vaibhavkale2002 <deshmukhvaibhav40872@gmail.com>
2026-08-14 18:11:12 +0530

    Update test_1.py

--- test_1.py
+++ test_1.py
@@ -19,14 +19,14 @@
 
 
 # P = int(input("Enter Principal: "))
-# R = int(input("Enter Rate: "))
+# R = float(input("Enter Rate: "))
 # T = int(input("Enter Time: "))
 
 # SI = (P * R * T) / 100
 
 # print("Simple Interest =", SI)
 
-#Q3
+#Q3 write a program to accept distance in km and convert it into meters and centimeters both
 # km = float(input("Enter distance in kilometers: "))
 
 # meters = km * 1000
@@ -35,4 +35,3 @@
 # print("Distance in meters =", meters)
 # print("Distance in centimeters =", centimeters)
 
-#Q4
